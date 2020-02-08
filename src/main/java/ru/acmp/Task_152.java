2087240cc27a6a07b921c1523b40c5338415e9a8
Andrey Savelev <andrsav2002@gmail.com>
2020-02-09 01:17:19 +0600

    second

---
+++ src/main/java/ru/acmp/Task_152.java
@@ -0,0 +1,48 @@
+package ru.acmp;
+
+import java.util.Scanner;
+
+class Task_152
+{
+    static final Scanner sc = new Scanner(System.in);
+
+    static boolean[][] mx = new boolean[100][100];
+    static boolean[] b = new boolean[100];
+
+    public static void main(String[] args)
+    {
+        final int n = sc.nextInt();
+        final int m = sc.nextInt();
+        for (int i = 0; i < m; ++i)
+        {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            a--;
+            b--;
+            mx[a][b] = true;
+        }
+        for (int a = 0; a < n; ++a)
+        {
+            Search(a, n);
+        }
+        System.out.println("Yes");
+    }
+
+    static void Search(int a, int n)
+    {
+        b[a] = true;
+        for (int i = 0; i < n; ++i)
+        {
+            if (!mx[a][i])
+            {
+                continue;
+            }
+            if (b[i]) {
+                System.out.println("No");
+                System.exit(0);
+            }
+            Search(i, n);
+        }
+        b[a] = false;
+    }
+}
